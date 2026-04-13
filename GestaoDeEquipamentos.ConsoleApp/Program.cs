77058e9c67ed5990888eb2216f1f45c45def704a
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-04-13 16:40:28 -0300

    adicionada: estrutura básica do program.cs

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,9 +1,50 @@
-﻿namespace GestaoDeEquipamentos.ConsoleApp;
+﻿using System.Security.Cryptography;
+
+namespace GestaoDeEquipamentos.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, mundo!");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Gestão de Equipamentos");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1 - Cadastrar equipamento");
+            Console.WriteLine("S - Sair");
+            Console.WriteLine("---------------------------------");
+            Console.Write("> ");
+            string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+            if (opcaoMenu == "S")
+            {
+                Console.Clear();
+                break;
+            }
+
+            if (opcaoMenu == "1")
+            {
+                
+            }
+
+            else if (opcaoMenu == "2")
+            {
+
+            }
+
+            else if (opcaoMenu == "3")
+            {
+
+            }
+
+            else if (opcaoMenu == "4")
+            {
+
+            }
+
+
+        }
     }
 }
