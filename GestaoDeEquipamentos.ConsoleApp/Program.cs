29c6f6c50e861afdfd4daff4be97072ee5a54bfb
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-04-23 13:50:26 -0300

    refactor: reset project to basic structure

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,19 +1,23 @@
-﻿using GestaoDeEquipamentos.ConsoleApp.Apresentacao;
-using GestaoDeEquipamentos.ConsoleApp.Dominio;
-using System.Security.Cryptography;
-
-namespace GestaoDeEquipamentos.ConsoleApp;
+﻿namespace GestaoDeEquipamentos.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Equipamento?[] equipamentos = new Equipamento[100]; //array de objetos
-        TelaEquipamento telaEquipamento = new TelaEquipamento(); //para usar os métodos da class
-
         while (true)
         {
-            string? opcaoMenu = telaEquipamento.ObterEscolhaDoMenuPrincipal();
+            Console.Clear();
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Gestão de Equipamentos");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1 - Cadastrar equipamento");
+            Console.WriteLine("2 - Editar equipamento");
+            Console.WriteLine("3 - Excluir equipamento");
+            Console.WriteLine("4 - Visualizar equipamentos");
+            Console.WriteLine("S - Sair");
+            Console.WriteLine("---------------------------------");
+            Console.Write("> ");
+            string? opcaoMenu = Console.ReadLine()?.ToUpper();
 
             if (opcaoMenu == "S")
             {
@@ -22,19 +26,16 @@ class Program
             }
 
             if (opcaoMenu == "1")
-                telaEquipamento.Cadastrar(equipamentos);
-            
+            {
+            }
             else if (opcaoMenu == "2")
-                telaEquipamento.Editar(equipamentos);
-
+            {
+            }
             else if (opcaoMenu == "3")
             {
-                telaEquipamento.Excluir(equipamentos);
             }
-
             else if (opcaoMenu == "4")
             {
-                telaEquipamento.Visualizar(equipamentos);
             }
         }
     }
